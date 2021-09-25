95d11c805fdf59a2591e02a5b097714622616bbe
Dasun Vimukthi <dasunvimukthi1998@gmail.com>
2021-09-26 03:12:17 +0530

    Validations done, quantity added for the cart insert

--- app/src/main/java/com/example/madmini/it20122096/DashBoard.java
+++ app/src/main/java/com/example/madmini/it20122096/DashBoard.java
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.example.madmini.R;
 import com.example.madmini.it20122096.BulidPc;
+import com.example.madmini.it20122096.spare_parts_comp.AddSparePart;
 import com.example.madmini.it20122096.spare_parts_comp.ShowSpareParts;
+import com.example.madmini.it20122096.spare_parts_comp.ViewAllSpareParts;
 
 public class DashBoard extends AppCompatActivity {
 
     ImageButton laptop,parts,build,profile;
+    Button addSparePart, manageSparePart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,10 @@ public class DashBoard extends AppCompatActivity {
         parts =(ImageButton) findViewById(R.id.parts_btn);
         build =(ImageButton) findViewById(R.id.build_btn);
         profile =(ImageButton) findViewById(R.id.profile_btn);
+
+        addSparePart = findViewById(R.id.btn_add_spare);
+        manageSparePart = findViewById(R.id.btn_manage_spare);
+
 
         build.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +44,16 @@ public class DashBoard extends AppCompatActivity {
 
         parts.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), ShowSpareParts.class);
+            startActivity(intent);
+        });
+
+        addSparePart.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), AddSparePart.class);
+            startActivity(intent);
+        });
+
+        manageSparePart.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), ViewAllSpareParts.class);
             startActivity(intent);
         });
     }
